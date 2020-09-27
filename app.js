0d1aa178d6e5ee1ec5ac21037b7fecc8be754c8d
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-09-27 10:49:26 +0100

    Git project done

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
+require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const fs = require('fs')
-const { Img } = require('./models/img')
 const app = express()
+
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
-const dbURI = 'mongodb+srv://netninja:test1234@cluster0.dfuhn.mongodb.net/imgUploader?retryWrites=true&w=majority'
 
+const cloudinary = require('cloudinary')
+const UPLOAD_PRESET = "xjjikdvo";
+const fs = require('fs')
+
+
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded())
-
-mongoose.connect(dbURI).then((result) => {
-    app.listen(3000, () => {
-        console.log('Server Running')
-    })
-})
 
 app.get('/', (req, res) => {
     res.sendFile('./public/HTML/index.html', { root: __dirname })
 })
 
 app.post('/upload', upload.single('imgInput'), (req, res, next) => {
-    const imgName = req.originalname
-    const filePath = req.file.destination + '/' + req.file.filename
-
-    fs.readFile(filePath, (err, data) => {
+    fs.readdir('./uploads/', (err, files) => {
         if (err) {
-            console.log(err.message)
-        } else {
-            Img.deleteMany().then(result => {
-                const newImg = new Img({
-                    data: data,
-                    name: req.file.originalname
-                })
-
-                newImg.save().then((result) => {
-                    console.log('Img Saved')
-                    res.sendFile('./public/HTML/uploaded.html', { root: __dirname })
-                }).catch(err => console.log(err.message))
-            }).catch(err => console.log(err.message))
+            throw err;
         }
+
+        fs.copyFile(`./uploads/${req.file.filename}`, `./uploads/${req.file.originalname}`, (err) => {
+            if (err) {
+                console.log(err.message)
+            }
+
+            cloudinary.v2.uploader.unsigned_upload(`./uploads/${req.file.originalname}`, UPLOAD_PRESET, { cloud_name: 'drh6wwwtw' }).then((response) => {
+
+                for (const file of files) {
+                    fs.unlink('./uploads/' + file, (error) => console.log(error))
+                }
+
+                res.render('uploaded.ejs', { imgUrl: response.url })
+            }).catch((error) => console.log(error.message))
+        })
     })
+})
+
+app.listen(3000, () => {
+    console.log('Server Running')
 })
--- public/JS/index.js
+++ public/JS/index.js
@@ -1,30 +1,37 @@
 const fileInputBigConteiner = document.querySelector('.invisible-file-uploader')
 const fileInputButton = document.querySelector('.real-input-button')
 const submitBtn = document.querySelector('#submit-btn')
+const filterLayer = document.querySelector('.effect-filter')
+const progressBar = document.querySelector('.progress-bar')
+let progressBarProgress = 0
 const fileReader = new FileReader()
 const img = new Image();
 let imgName = ''
+
+
+
+function runUploadEffect(){
+    progressBar.style.width = progressBarProgress + '%';
+    progressBarProgress += 10
+}
+
 
 fileInputBigConteiner.onchange = () => {
     fileReader.onload = () => {
         img.src = fileReader.result
         document.querySelector('.img-preview').src = img.src
         imgName = fileInputBigConteiner.files[0].name;
-        submitBtn.style.display = 'block'
+        
+        filterLayer.style.display = 'block'
+        setInterval(runUploadEffect, 1000)
+
+        setTimeout(()=>{
+            submitBtn.click()
+        }, 10000)
     }
     fileReader.readAsDataURL(fileInputBigConteiner.files[0])
 }
 
 fileInputButton.onchange = () => {
-    fileReader.onload = () => {
-        img.src = fileReader.result
-        document.querySelector('.img-preview').src = img.src
-        imgName = fileInputBigConteiner.files[0].name;
-        submitBtn.style.display = 'block'
-    }
-    fileReader.readAsDataURL(fileInputButton.files[0])
+    fileInputBigConteiner.files = fileInputButton.files
 }
-
-submitBtn.addEventListener('click', ()=>{
-    window.location.assign(`/upload-img/${img.src}/${imgName}`)
-})
